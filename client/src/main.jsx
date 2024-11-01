953758a25788a7740b17a1e83e05e6e74d82e2da
Karl-Xavier <uwasbruno256@gmail.com>
2024-11-01 13:28:24 +0100

    Added Vercel Tracking

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 
 createRoot(document.getElementById('root')).render(
   <Router>
     <StrictMode>
       <App />
+      <Analytics/>
     </StrictMode>
   </Router>,
 )
