3e7af99b9db029125296458b7e660edff64be2c7
Karl-Xavier <uwasbruno256@gmail.com>
2024-11-22 11:32:20 +0100

    secured

--- server/admin.js
+++ server/admin.js
@@ -1,8 +1,10 @@
 const admin = require('firebase-admin')
-const serviceAccount = require('./config/shonenstream-48a83-firebase-adminsdk-copu3-81dbcdabef.json')
+//const serviceAccount = require('./config/shonenstream-48a83-firebase-adminsdk-copu3-81dbcdabef.json')
+const path = JSON.parse(process.env.FIREBASE_ADMIN_CREDIENTIALS)
+console.log(path)
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert(path)
 })
 
 module.exports = admin
